7bd789bd485d20aab33406aa29ce889c4e03a383
ultravpnhost <ultravpnhosting@gmail.com>
2026-06-25 19:18:41 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,185 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  // Полный массив твоих конфигураций серверов для Happ
+  const cleanJson = `[
+  {
+    "protocol": "vless",
+    "remarks": "🇩🇪 Германия⚡",
+    "streamSettings": {
+      "network": "tcp",
+      "security": "reality",
+      "realitySettings": {
+        "fingerprint": "qq",
+        "publicKey": "r6lN34m1nN-xQZ458j5NPD5xJ3_QBF2bGzY4KJEo4ic",
+        "serverName": "ads.x5.ru",
+        "shortId": "abbcd128"
+      }
+    },
+    "settings": {
+      "address": "de-new.datanode-internal.net",
+      "encryption": "none",
+      "flow": "xtls-rprx-vision",
+      "id": "9d5e7e04-53e4-4d98-bb26-236c907078a5",
+      "level": 8,
+      "port": 443
+    }
+  },
+  {
+    "protocol": "vless",
+    "remarks": "🇸🇪 Швеция⚡",
+    "streamSettings": {
+      "network": "tcp",
+      "security": "reality",
+      "realitySettings": {
+        "fingerprint": "qq",
+        "publicKey": "r6lN34m1nN-xQZ458j5NPD5xJ3_QBF2bGzY4KJEo4ic",
+        "serverName": "ads.x5.ru",
+        "shortId": "abbcd128"
+      }
+    },
+    "settings": {
+      "address": "se-new.datanode-internal.net",
+      "encryption": "none",
+      "flow": "xtls-rprx-vision",
+      "id": "9d5e7e04-53e4-4d98-bb26-236c907078a5",
+      "level": 8,
+      "port": 443
+    }
+  },
+  {
+    "protocol": "vless",
+    "remarks": "🇵🇱 Польша",
+    "streamSettings": {
+      "network": "tcp",
+      "security": "reality",
+      "realitySettings": {
+        "fingerprint": "qq",
+        "publicKey": "r6lN34m1nN-xQZ458j5NPD5xJ3_QBF2bGzY4KJEo4ic",
+        "serverName": "sun9-35.userapi.com",
+        "shortId": "abbcd128"
+      }
+    },
+    "settings": {
+      "address": "pl.datanode-internal.net",
+      "encryption": "none",
+      "flow": "xtls-rprx-vision",
+      "id": "9d5e7e04-53e4-4d98-bb26-236c907078a5",
+      "level": 8,
+      "port": 443
+    }
+  },
+  {
+    "protocol": "vless",
+    "remarks": "🇷🇺 Россия",
+    "streamSettings": {
+      "network": "tcp",
+      "security": "reality",
+      "realitySettings": {
+        "fingerprint": "qq",
+        "publicKey": "r6lN34m1nN-xQZ458j5NPD5xJ3_QBF2bGzY4KJEo4ic",
+        "serverName": "sun9-38.userapi.com",
+        "shortId": "abbcd128"
+      }
+    },
+    "settings": {
+      "address": "ru.datanode-internal.net",
+      "encryption": "none",
+      "flow": "xtls-rprx-vision",
+      "id": "9d5e7e04-53e4-4d98-bb26-236c907078a5",
+      "level": 8,
+      "port": 443
+    }
+  },
+  {
+    "protocol": "vless",
+    "remarks": "🇩🇪 LTE #1 ⚡",
+    "streamSettings": {
+      "network": "tcp",
+      "security": "reality",
+      "realitySettings": {
+        "fingerprint": "qq",
+        "publicKey": "r6lN34m1nN-xQZ458j5NPD5xJ3_QBF2bGzY4KJEo4ic",
+        "serverName": "ads.x5.ru",
+        "shortId": "abbcd128"
+      }
+    },
+    "settings": {
+      "address": "hole3.datanode-internal.net",
+      "encryption": "none",
+      "flow": "xtls-rprx-vision",
+      "id": "9d5e7e04-53e4-4d98-bb26-236c907078a5",
+      "level": 8,
+      "port": 9443
+    }
+  },
+  {
+    "protocol": "vless",
+    "remarks": "🇵🇱 LTE #2 ",
+    "streamSettings": {
+      "network": "tcp",
+      "security": "reality",
+      "realitySettings": {
+        "fingerprint": "firefox",
+        "publicKey": "noOR7PAxYWRRrbQywDahrBlTkbNuNFkMLCPcX-wfa5TY",
+        "serverName": "a.wb.ru",
+        "shortId": "086007"
+      }
+    },
+    "settings": {
+      "address": "178.250.242.194",
+      "encryption": "none",
+      "flow": "xtls-rprx-vision",
+      "id": "b92337e0-6819-4f17-a3cc-f860b138b27a",
+      "level": 8,
+      "port": 6443
+    }
+  },
+  {
+    "protocol": "vless",
+    "remarks": "🇳🇱 LTE #3 ",
+    "streamSettings": {
+      "network": "tcp",
+      "security": "reality",
+      "realitySettings": {
+        "fingerprint": "firefox",
+        "publicKey": "grRXV9R0CYdwQX8R4S1qfMde1b8g6Ejr1UZCxM5-PkM",
+        "serverName": "api.ok.ru",
+        "shortId": "3efa66e0"
+      }
+    },
+    "settings": {
+      "address": "178.250.242.194",
+      "encryption": "none",
+      "flow": "xtls-rprx-vision",
+      "id": "7573908b-d981-4153-92aa-33ee2d22144c",
+      "level": 8,
+      "port": 443
+    }
+  }
+]`;
+
+  const newHeaders = new Headers();
+  
+  // 1. Формат JSON UTF-8
+  newHeaders.set("Content-Type", "application/json; charset=utf-8");
+  
+  // 2. Название подписки: "Ultra VPN 🏳️" (закодировано в Base64 с эмодзи)
+  newHeaders.set("profile-title", "base64:VWx0cmEgVlBOIPCfh7Y=");
+  
+  // 3. Лимиты трафика: Осталось 357 ГБ / Бесконечность, окончание 13.03.2030
+  newHeaders.set("subscription-userinfo", "upload=0; download=383331401728; total=0; expire=1899589200");
+  
+  // 4. Обновление каждый 1 час
+  newHeaders.set("profile-update-interval", "1");
+  
+  // 5. Разрешение CORS для любых клиентов
+  newHeaders.set("Access-Control-Allow-Origin", "*");
+
+  return new Response(cleanJson, {
+    status: 200,
+    headers: newHeaders
+  });
+}
